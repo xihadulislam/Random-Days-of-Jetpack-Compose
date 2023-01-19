c0e4321ab6fb06bc52088c967e8443cd21957a80
ziad <ziad@yotech.ltd>
2023-01-19 15:06:04 +0600

    init commit

---
+++ app/src/main/java/com/xihadulislam/randomjetpackcompose/MainActivity.kt
@@ -0,0 +1,145 @@
+package com.xihadulislam.randomjetpackcompose
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.xihadulislam.randomjetpackcompose.ui.theme.RandomjetpackcomposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RandomjetpackcomposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    UserList()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun  ImageCard(
+    painter: Painter,
+    contentDescription:String,
+    title: String,
+    modifier: Modifier = Modifier
+
+){
+
+
+
+
+}
+
+
+
+@Composable
+fun UserList() {
+
+//    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+//        for (i in 1..10) {
+//            UserCard()
+//        }
+//    }
+
+    LazyColumn {
+        items((1..20).toList()) { user ->
+            UserCard()
+        }
+    }
+
+}
+
+
+@Composable
+fun UserCard() {
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(12.dp)
+            .border(width = 1.dp, color = Color.Gray)
+            .padding(12.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "",
+            modifier = Modifier
+                .size(100.dp)
+                .clip(CircleShape),
+            contentScale = ContentScale.Crop
+        )
+        Spacer(modifier = Modifier.padding(6.dp))
+
+        Column(modifier = Modifier.padding(6.dp)) {
+
+            Text(text = "Xihad Islam", fontSize = 18.sp, fontWeight = FontWeight.Bold)
+
+            Spacer(modifier = Modifier.padding(6.dp))
+
+            Text(text = stringResource(id = R.string.dummy))
+
+            Spacer(modifier = Modifier.padding(8.dp))
+
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "View Profile")
+            }
+        }
+    }
+}
+
+
+@Composable
+fun Greeting(name: String) {
+
+    val context = LocalContext.current
+    Text(
+        text = "Hello $name!",
+        fontSize = 32.sp,
+        color = colorResource(R.color.purple_700),
+        fontFamily = FontFamily.Cursive,
+        modifier = Modifier.clickable {
+            Toast.makeText(context, "you click me ", Toast.LENGTH_SHORT).show()
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Surface(Modifier.fillMaxSize()) {
+        UserList()
+    }
+}
